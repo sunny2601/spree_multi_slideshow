7a01569121d66a1f5cae96669c665f56305a9e8a
sunny2601 <sunnysarrot@gmail.com>
2015-06-23 10:13:13 +0530

    Update 20120116081431_create_slideshows.rb

--- db/migrate/20120116081431_create_slideshows.rb
+++ db/migrate/20120116081431_create_slideshows.rb
@@ -1,6 +1,6 @@
 class CreateSlideshows < ActiveRecord::Migration
   def change
-    create_table :slideshows do |t|
+    create_table :spree_slideshows do |t|
       t.string :category
       t.boolean :enabled, :default => false
       t.string :mode, :default => 'horizontal'
